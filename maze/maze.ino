0ad93847e037fc9449a26811ec650f081d0018af
roxanaionescu96 <32487348+roxanaionescu96@users.noreply.github.com>
2018-02-27 14:49:21 +0200

    Add files via upload

--- maze/maze.ino
+++ maze/maze.ino
@@ -1,41 +1,74 @@
 int sensorPin1=GPP_0;  
 int sensorPin2=GPP_1;
 int sensorPin3=GPP_3;
-int leftMotorSpeed=150,rightMotorSpeed=150;
-int  frontSensor,leftSensor,RightSensor; 
+
+int leftMotorSpeed=150;
+int rightMotorSpeed=150;
+char moveInMaze[1000];//l-left,r-right,f-forward
+int index=0;
+
+//sensor read ??? asynchronus check,have to put delay or not    
+int frontSensor;
+int leftSensor;             
+int rightSensor;
+
+  
+void stopMotors(){
+  digitalWrite(PWM_1,LOW) ;
+  digitalWrite(PWM_2,LOW) ;
+  digitalWrite(PWM_3,LOW) ;
+  digitalWrite(PWM_4,LOW) ;
+}
+
+
+//check if the cell is black or white
+boolean checkCell(){
+   return (digitalRead(IO_0))==1;
+}
 
 void moveForward(){
+  moveInMaze[index]='f';
+  index++;
   digitalWrite(PWM_1,LOW) ;
   digitalWrite(PWM_2,HIGH) ;
   digitalWrite(PWM_3,LOW) ;
   digitalWrite(PWM_4,HIGH) ;
-  analogWrite(PWM_0,rightMotorSpeed);
-  analogWrite(PWM_5,leftMotorSpeed);
+  analogWrite(PWM_0,rightMotorSpeed+calculatePID());
+  analogWrite(PWM_5,leftMotorSpeed-calculatePID());
+}
+
+int calculatePID(){
+  //have to find values for them
+  int kp;
+  int kd;
+  return (leftSensor-rightSensor)*kp+(rightSensor-leftSensor)*kd;
+    
 }
 void moveLeft(){
+  moveInMaze[index]='l';
+  index++;
   digitalWrite(PWM_1,LOW) ;
   digitalWrite(PWM_2,HIGH) ;
   digitalWrite(PWM_3,HIGH) ;
   digitalWrite(PWM_4,LOW) ;
   analogWrite(PWM_0,rightMotorSpeed);
   analogWrite(PWM_5,leftMotorSpeed);
+  delay(100);
+  stopMotors();
 }
 void moveRight(){
+  moveInMaze[index]='r';
+  index++;
   digitalWrite(PWM_1,HIGH) ;
   digitalWrite(PWM_2,LOW) ;
   digitalWrite(PWM_3,LOW) ;
   digitalWrite(PWM_4,HIGH) ;
   analogWrite(PWM_0,rightMotorSpeed);
   analogWrite(PWM_5,leftMotorSpeed);
+  delay(100);
+  stopMotors();
 }
-void moveBackward(){
-  digitalWrite(PWM_1,HIGH) ;
-  digitalWrite(PWM_2,LOW) ;
-  digitalWrite(PWM_3,HIGH) ;
-  digitalWrite(PWM_4,LOW) ;
-  analogWrite(PWM_0,rightMotorSpeed);
-  analogWrite(PWM_5,leftMotorSpeed);
-}
+
 void setup() {
   Serial.begin(9600);                         
   Serial.println("Example: Single HC-SR04");  
@@ -45,12 +78,35 @@ void setup() {
 }
 
 void loop()
-{                 
+{  
+            
   frontSensor=Ultrasonic.read(sensorPin1); 
   leftSensor=Ultrasonic.read(sensorPin2);              
-  rightSensor=Ultrasonic.read(sensorPin3);             
-  if(leftSensor>26) {
-    moveLeft();
-  }
+  rightSensor=Ultrasonic.read(sensorPin3);
+  
+  //first cell to check if black 
+ // while(((checkCell())==false)){
+ //   if(firstCellCheckExcep==1) break;
+ // }
+
+ 
+  //no left wall case
+    if(leftSensor>26) {
+                      moveLeft();
+                      moveForward();
+                      }
+        else if(frontSensor>26){
+                                moveForward();
+                              }
+            else if(rightSensor>26){
+                                    moveRight();
+                                    moveForward();
+                                    }
+                   else {
+                         moveRight();
+                         moveRight();
+                         moveForward();
+                        }
+                    
   delay(100);                                 
 }
